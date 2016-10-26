1f5227b0685e8ad44bb0c330706ff9c3091acb51
David Rees <d.w.rees.03@gmail.com>
2016-10-26 12:34:27 +0100

    First commit

---
+++ mail.php
@@ -0,0 +1,51 @@
+<?php
+  
+  require(__DIR__.'/recaptcha/src/autoload.php');  
+  require(__DIR__.'/recaptcha/src/secrets.php');  
+  
+  $recaptcha = new \ReCaptcha\ReCaptcha($secret);
+  $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+ if ($resp->isSuccess()):
+	
+	/*
+		The Send Mail php Script for Contact Form
+		Server-side data validation is also added for good data validation.
+	*/
+	
+	$data['error'] = false;
+	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+	
+	if( empty($name) ){
+		$data['error'] = 'Please enter your name.';
+	}else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+		$data['error'] = 'Please enter a valid email address.';
+	}else if( empty($subject) ){
+		$data['error'] = 'Please enter your subject.';
+	}else if( empty($message) ){
+		$data['error'] = 'The message field is required!';
+	}else{
+		
+		$formcontent="From: $name\nSubject: $subject\nEmail: $email\nMessage: $message";
+		
+		
+		//Place your Email Here
+		$recipient = "dwrees03+drmusicsite@gmail.com";
+		
+		$mailheader = "From: $email \r\n";
+		
+		if( mail($recipient, $name, $formcontent, $mailheader) == false ){
+			$data['error'] = 'Sorry, an error occured!';
+		}else{
+			$data['error'] = false;
+		}
+	
+	}
+	
+	echo json_encode($data);
+	endif;
+	
+?>
